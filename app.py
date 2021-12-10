08b65517bf2f4e50ad445791edc3daa8b7339a79
Narasimha Rao <39879516+narsimrao@users.noreply.github.com>
2021-12-10 10:53:14 +0530

    server line added

--- app.py
+++ app.py
@@ -439,6 +439,8 @@ print('Wait for the browser to load....')
 
 BS = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
 app = dash.Dash(__name__,external_stylesheets=[BS,dbc.themes.BOOTSTRAP])
+server = app.server
+
 app.layout = html.Div([
     ###########Q1 Header###########
     html.H2(
